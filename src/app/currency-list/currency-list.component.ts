5bd9806946fcf3142284e941006d57d7cf10792d
Gabriella Sinka Pálinkás <fusewent@gmail.com>
2019-03-18 00:58:39 +0100

    some refactoring

--- src/app/currency-list/currency-list.component.ts
+++ src/app/currency-list/currency-list.component.ts
@@ -11,6 +11,7 @@ import { Observable } from 'rxjs';
 export class CurrencyListComponent implements OnInit {
 
   Currencies: any = [];
+  symbols: any = ["btc", "xrp", "eth"];
 
   constructor(
     public restApi: RestApiService
@@ -21,13 +22,13 @@ export class CurrencyListComponent implements OnInit {
   }
 
   loadCurrencies() {
-    this.restApi.getExchangeRate("btc").subscribe((data: {}) => {
-      this.Currencies.push(data);
-    })
-    this.restApi.getExchangeRate("xrp").subscribe((data: {}) => {
-      this.Currencies.push(data);
-    })
-    this.restApi.getExchangeRate("eth").subscribe((data: {}) => {
+    this.symbols.forEach(symbol => {
+      this.pushCurrencyBySymbol(symbol);
+    });
+  }
+
+  pushCurrencyBySymbol(symbol) {
+    this.restApi.getExchangeRate(symbol).subscribe((data: {}) => {
       this.Currencies.push(data);
     })
   }
